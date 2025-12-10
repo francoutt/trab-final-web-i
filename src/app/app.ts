e0f7791f8d74f0b57f60bc2f8b98c2c1887b81b2
Francis <francis.couto.rocha@gmail.com>
2025-12-09 21:14:02 -0300

    atualização app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,13 +7,7 @@ import { ProductList } from './components/product-list/product-list';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    Header,
-    Footer,
-    SearchBar,
-    ProductList
-  ],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
